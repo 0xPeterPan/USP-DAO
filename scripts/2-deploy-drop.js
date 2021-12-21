e48d39393c9251a52dae744fff6d9ed8bb4ad561
0xPeterPan <abdulmalek83@gmail.com>
2021-12-21 09:44:50 +0300

    spelling correction

--- scripts/2-deploy-drop.js
+++ scripts/2-deploy-drop.js
@@ -12,7 +12,7 @@ const app = sdk.getAppModule("0x6b769Aa6C11660a7695698dD35BD35866EbCc5a6");
       // A description for the collection.
       description: "This DAO is where you can show your solidarity with Palestinians",
       // The image for the collection that will show up on OpenSea.
-      image: readFileSync("scripts/assets/Key og Return.jpg"),
+      image: readFileSync("scripts/assets/Key of Return.jpg"),
       // We need to pass in the address of the person who will be receiving the proceeds from sales of nfts in the module.
       // We're planning on not charging people for the drop, so we'll pass in the 0x0 address
       // you can set this to your own wallet address if you want to charge for the drop.
